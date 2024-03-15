9a2d9204fd0620a99cc2386496a99a195f83054f
Bmo037 <wkgid00723@gmail.com>
2024-03-15 15:10:06 +0900

    vi test

---
+++ shells/logic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+opt=$1
+
+	if [ $opt == 'test' -o $opt == 'add' ]; then
+		echo good 
+else
+		echo bad 
+		fi
+	
---
+++ shells/logic_new.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+opt=$1
+opt=$2
+
+if [ $# -eq ]; then
+	if [ $opt1 == 'test' -a $opt2 == 'aaa' ]; then
+		echo good
+	if [ $opt1 == 'aaa' -a $opt2 == 'test' ]; then
+		echo good 
+else
+		echo bad 
+		fi
+else
+	echo "Input two prameters....!!"
+	fi
+
